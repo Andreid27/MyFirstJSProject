ce8ea84eec70f7d42dd46cc205bdfd84a91e391a
Andrei27 <andrei@dinca.one>
2021-12-05 01:27:26 +0200

    Detectare eroare field complete + inceput stil css
    Dar mai este de terminat stil css +
    de rezolvat cazult in care secompleteaza eroarea

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
  let lastEl = ""
+ let FirstName = document.querySelector('#FirstName')
+ let LastName = document.querySelector('#LastName')
+ let EmailAdress = document.querySelector('#EmailAdress')
+ let Password = document.querySelector('#Password')
+ let vectorInput = [FirstName,LastName,EmailAdress,Password]
+ let errValid= false
  
  function makeDefault(element){
        lastEl=element.value
@@ -9,38 +15,89 @@
        // console.log(element)
        }
  }
+ function makeError(element){
+      lastEl=element.value
+      if (element.value==="First Name" || element.value==="Last Name" || element.value==="Email Adress"||element.value==="Password") 
+      { element.value = ``
+       element.classList.remove("defaultText")
+       element.classList.add("Error-color")
+       element.style.borderColor = "red"
+      // console.log(element)
+      }
+}
 
  function restoreDefault(){
-       if(document.querySelector('#FirstName').value===``)
-       {element = document.querySelector('#FirstName')
+       if(FirstName.value===``)
+       {element = FirstName
        element.value=lastEl
        element.classList.remove("ActiveText")
        element.classList.add('defaultText')}
        
-       if(document.querySelector('#LastName').value===``)
-       {element = document.querySelector('#LastName')
+       if(LastName.value===``)
+       {element = LastName
        element.value=lastEl
        element.classList.remove("ActiveText")
        element.classList.add('defaultText')}
        
-       if(document.querySelector('#EmailAdress').value===``)
-       {element = document.querySelector('#EmailAdress')
+       if(EmailAdress.value===``)
+       {element = EmailAdress
        element.value=lastEl
        element.classList.remove("ActiveText")
        element.classList.add('defaultText')}
               
-       if(document.querySelector('#Password').value===``)
-       {element = document.querySelector('#Password')
+       if(Password.value===``)
+       {element = Password
        element.value=lastEl
        element.classList.remove("ActiveText")
        element.classList.add('defaultText')}
 
      } 
 
- document.querySelector('#FirstName').addEventListener('click',function(e) {e.preventDefault(); makeDefault(document.querySelector('#FirstName'))})
- document.querySelector('#LastName').addEventListener('click',function(e) {e.preventDefault(); makeDefault(document.querySelector('#LastName'))})
- document.querySelector('#EmailAdress').addEventListener('click',function(e) {e.preventDefault(); makeDefault(document.querySelector('#EmailAdress'))})
- document.querySelector('#Password').addEventListener('click',function(e) {e.preventDefault(); makeDefault(document.querySelector('#Password'))})
+       FirstName.addEventListener('click',function(e) {e.preventDefault(); makeDefault(FirstName)})
+       LastName.addEventListener('click',function(e) {e.preventDefault(); makeDefault(LastName)})
+       EmailAdress.addEventListener('click',function(e) {e.preventDefault(); makeDefault(EmailAdress)})
+       Password.addEventListener('click',function(e) {e.preventDefault(); makeDefault(Password)})
+       document.getElementById("start").addEventListener('click',function(e) {e.preventDefault(); checkComplete()})
+       document.body.addEventListener('click', function(e) {e.preventDefault(); if (errValid===false) {restoreDefault()}}, true)
+       function validateForm(element) {
+           // Declare all the local variable
+           let inputElements, inputId, inputType, i, inputLength, inputNode;
+           //modificarea mea la declarari
+           let inputValue
+       
+           inputElements = document.getElementsByTagName("input");
+       
+           //for (i = 0, inputLength = inputElements.length; i < inputLength; i++) {
+               inputId = element.id;
+               //inputType = inputElements[i].type;
+               inputValue=element.value       
+               //if (inputType === "text") {
+                 if (inputValue==="First Name" || inputValue==="Last Name" || inputValue==="Email Adress" ||inputValue==="Password" ||inputValue===`` ) {
+                   inputNode = document.getElementById(inputId);
+                   //if (inputNode.value === "") {
+                       var spanTag = document.createElement("span");
+                       var imgTag = document.createElement("img");
+                       imgTag.src = "./images/icon-error.svg"
+                       imgTag.classList.add("error-msg")
+                       spanTag.innerHTML = `<span class="error-msg">${element.value} cannot be empty</span>`
+                       inputNode.parentNode.insertBefore(spanTag, inputNode.nextSibling)
+                       inputNode.parentNode.insertBefore(imgTag, inputNode.nextSibling)
+                       makeError(element)
+                       errValid=true
+                  console.log("sa moara jiubilan daca mai intra") 
+                 // }
+                  }
+          // }
+           console.log(errValid)
 
- document.body.addEventListener('click', function(e) {e.preventDefault(); restoreDefault()}, true)
+           return false; // Do Nothing
+       
+       }
+       console.log("mesaj2"+errValid)
 
+function checkComplete(){
+            for(let i=0; i<4;i++)
+            validateForm(vectorInput[i])
+              
+
+}
